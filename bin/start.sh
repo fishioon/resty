db5c63ca1f5a5ec384f4fbd6aa02854042f0e0c4
fishioon <fishioon@live.com>
2016-06-27 16:09:45 +0800

    delete file

--- bin/start.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-/usr/local/openresty/nginx/sbin/nginx -t -c /Users/fish/Github/resty/conf/nginx.conf
-/usr/local/openresty/nginx/sbin/nginx -p /Users/fish/Github/resty/ 
--- bin/stop.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-/usr/local/openresty/nginx/sbin/nginx -p /Users/fish/Github/resty/ -s stop
